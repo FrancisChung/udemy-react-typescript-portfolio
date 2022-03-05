1c47329777aea173da935a037aaaadf8f3961c10
FrancisChung <francis.c.chung@gmail.com>
2022-03-05 14:24:05 +1100

    Parent to handle OnClick

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,7 @@
 import {Child} from './Child'
 
 const Parent = () => { 
-    return <Child color="blue" />
+    return <Child color="blue" onClick={() => console.log("OnClick Clicked") } />
 }
 
 export default Parent;
